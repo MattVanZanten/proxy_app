a5804f528e94a6a5a38fdd058b068bfcff368a0d
mattvanzanten@gmail.com <matt@archer.local>
2016-04-28 10:47:47 -0700

    conf

--- post.php
+++ post.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once('./config.php');
-if ( issset( $_GET['ip'] ) ) {
+if ( isset( $_GET['ip'] ) ) {
   $valid = preg_match( '/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\z/', $_GET['ip'] );
   if ( $valid === true ) {
     $ip_cidr = "{$_GET['ip']}/32";
