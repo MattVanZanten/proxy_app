281c8b6d2497d4abbd810c149065b7f9e64ffbac
matt vanzanten <mattvanzanten@gmail.com>
2016-04-28 17:34:53 +0000

    init

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+
+function get_ip() {
+		//Just get the headers if we can or else use the SERVER global
+		if ( function_exists( 'apache_request_headers' ) ) {
+			$headers = apache_request_headers();
+		} else {
+			$headers = $_SERVER;
+		}
+		//Get the forwarded IP if it exists
+		if ( array_key_exists( 'X-Forwarded-For', $headers ) && filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ) {
+			$the_ip = $headers['X-Forwarded-For'];
+		} elseif ( array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers ) && filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 )
+		) {
+			$the_ip = $headers['HTTP_X_FORWARDED_FOR'];
+		} else {
+			
+			$the_ip = filter_var( $_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 );
+		}
+		return $the_ip;
+}
+
+function check_ip_match( $cip ) {
+$file = '/etc/squid/s3/whitelist.txt';
+$searchfor = $cip;
+
+// get the file contents, assuming the file to be readable (and exist)
+$contents = file_get_contents($file);
+// escape special characters in the query
+$pattern = preg_quote($searchfor, '/');
+// finalise the regular expression, matching the whole line
+$pattern = "/^.*$pattern.*\$/m";
+// search, and store all matching occurences in $matches
+if(preg_match_all($pattern, $contents, $matches)){
+   return true;
+}
+else{
+   return false;
+}
+}
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once('./functions.php');
+$cip = get_ip();
+if( check_ip_match($cip) === false ) { 
+?>
+<html>
+<body>
+<form action="post.php" method="get">
+Your IP is: <input type="text" name="ip" value="<?php echo $cip ?>">
+<input type="submit">
+</form>
+
+</body>
+</html>
+<?php } else { ?>
+<html>
+<body>
+	<p>Your IP `<?php echo $cip ?>` is already in the system.</p>
+</body>
+</html>
+<?php } ?>
