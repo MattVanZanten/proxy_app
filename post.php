7c95c2b1abcd2ded89984a43951c97307d38d7a5
mattvanzanten@gmail.com <matt@archer.local>
2016-04-28 11:01:55 -0700

    added extra ===

--- post.php
+++ post.php
@@ -6,9 +6,9 @@ error_reporting(E_ALL);
 require_once('./config.php');
 require_once('./functions.php');
 if ( isset( $_GET['ip'] ) ) {
-  if ( check_ip_match($_GET['ip'], $file) === false ) {
+  if ( check_ip_match($_GET['ip'], $file) == false ) {
     $valid = preg_match( '/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $_GET['ip'] );
-    if ( $valid === true ) {
+    if ( $valid == true ) {
       $ip_cidr = "{$_GET['ip']}/32";
       file_put_contents($file, $ip_cidr, FILE_APPEND | LOCK_EX);
       echo "IP address successfully added to whitelist";
