3eda70e7dffe4fdba4b9b971a7864cfaf5a494e3
mattvanzanten@gmail.com <matt@archer.local>
2016-04-28 10:50:48 -0700

    regex

--- post.php
+++ post.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 require_once('./config.php');
 if ( isset( $_GET['ip'] ) ) {
-  $valid = preg_match( '/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\z/', $_GET['ip'] );
+  $valid = preg_match( '/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $_GET['ip'] );
   if ( $valid === true ) {
     $ip_cidr = "{$_GET['ip']}/32";
     file_put_contents($file, $ip_cidr);
