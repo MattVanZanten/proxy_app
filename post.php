a102c443a0e417d1ba8d0988c29c99ceef7e2970
mattvanzanten@gmail.com <matt@archer.local>
2016-04-28 10:56:56 -0700

    better error management

--- post.php
+++ post.php
@@ -4,13 +4,20 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once('./config.php');
+require_once('./functions.php');
 if ( isset( $_GET['ip'] ) ) {
-  $valid = preg_match( '/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $_GET['ip'] );
-  if ( $valid === true ) {
-    $ip_cidr = "{$_GET['ip']}/32";
-    file_put_contents($file, $ip_cidr);
+  if ( check_ip_match($_GET['ip'], $file) === true ) {
+    echo $$_GET['ip'];
+    $valid = preg_match( '/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $_GET['ip'] );
+    if ( $valid === true ) {
+      $ip_cidr = "{$_GET['ip']}/32";
+      file_put_contents($file, $ip_cidr, FILE_APPEND | LOCK_EX);
+      echo "IP address successfully added to whitelist";
+    } else {
+      echo 'IP address not valid';
+    }
   } else {
-    echo 'hi';
+    echo "IP address already in whitelist";
   }
 }
 ?>
